a1290bf123ce10de876410e15c1fb371093a88e1
Jason <jasonsoloriom@gmail.com>
2018-08-31 15:08:15 -0600

    After separating the navbar into its own component, i removed it from the root component and was able to import it and display it that way

--- src/index.js
+++ src/index.js
@@ -3,26 +3,15 @@
  import { BrowserRouter as Router, Link} from 'react-router-dom';
  import Route from 'react-router-dom/Route';
  import ReactDOM from 'react-dom';
- import Jumbo from './components/jumbotron'
- import 'bootstrap/dist/css/bootstrap.min.css';
- import {Navbar, NavbarBrand, NavLink, NavItem, Nav} from 'reactstrap';
+ import Jumbo from './components/jumbotron';
+ import NavBar from './components/navbar';
 
  class App extends Component {
    render(){
        return (
          <Router>
             <div>
-              <Navbar className="navbar navbar-light bg-primary" expand="lg">
-                <NavbarBrand href="/" className="text-white font-italic font-weight-bold">ReactRouter</NavbarBrand>
-                  <Nav className="mr-auto" navbar>
-                    <NavItem className="pull-left">
-                      <NavLink color="dark" href="/" className="text-white font-italic font-weight-bold">Home</NavLink>
-                    </NavItem>
-                    <NavItem className="float-left">
-                      <NavLink href="/about" className="text-white font-italic font-weight-bold">About</NavLink>
-                    </NavItem>
-                  </Nav>
-              </Navbar>
+              <NavBar/>
 
               <Route path="/" exact render={
                   ()=>{
